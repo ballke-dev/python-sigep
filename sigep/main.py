399a5bdfa3051d785c864a77aba4bc746e8dfd4a
Bruno Ribeiro <brunoribeiroinf@gmail.com>
2016-03-30 14:56:05 -0300

    limpeza de cep e conversão de mapas de caracteres iso-8859-1 <> utf-8

--- sigep/main.py
+++ sigep/main.py
@@ -2,6 +2,7 @@
 import os
 import xml.dom.minidom
 import urllib2
+from util import *
 
 SANDBOX_URL = 'https://apphom.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl'
 PRODUCTION_URL = 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl'
@@ -12,7 +13,7 @@ class BaseConnObject(object):
     tags = ['faultcode', 'faultstring']
 
     def __init__(self, sandbox=False):
-        self.headers = {'Content-Type': 'application/soap+xml; charset=utf-8'}
+        self.headers = {'Content-Type': 'application/soap+xml; charset=iso-8859-1'}
         self.url = SANDBOX_URL if sandbox else PRODUCTION_URL
 
 
@@ -30,14 +31,14 @@ class ConsultaCEP(BaseConnObject):
 
     def __init__(self, cep, sandbox=False):
         super(ConsultaCEP, self).__init__(sandbox=sandbox)
-        self.cep = cep
+        self.cep = clean_postcode(cep)
         self.tags += ['bairro', 'cep', 'cidade', 'complemento', 'complemento2', 'end', 'id', 'uf']
 
     def do(self):
         self.documento = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.template), 'r').read() % self.__dict__
         self.requisicao = urllib2.Request(self.url, self.documento, self.headers)
         self.resposta = urllib2.urlopen(self.requisicao)
-        self.conteudo = self.resposta.read()
+        self.conteudo = iso_2_utf(self.resposta.read())
 
         self.dom = xml.dom.minidom.parseString(self.conteudo)
 
@@ -45,7 +46,8 @@ class ConsultaCEP(BaseConnObject):
 
         for tag in self.tags:
             try:
-                dados[tag] = self.dom.getElementsByTagName(tag)[0].childNodes[0].data
+                cont = self.dom.getElementsByTagName(tag)[0].childNodes[0].data
+                dados[tag] = cont
             except IndexError:
                 pass
 
--- sigep/util.py
+++ sigep/util.py
@@ -1,50 +1,23 @@
-from decimal import Decimal
+# -*- encoding: utf-8 -*-
+import re
 
-def moneyfmt(value, places=2, curr='', sep=',', dp='.',
-             pos='', neg='-', trailneg=''):
-    """Convert Decimal to a money formatted string.
 
-    places:  required number of places after the decimal point
-    curr:    optional currency symbol before the sign (may be blank)
-    sep:     optional grouping separator (comma, period, space, or blank)
-    dp:      decimal point indicator (comma or period)
-             only specify as blank when places is zero
-    pos:     optional sign for positive numbers: '+', space or blank
-    neg:     optional sign for negative numbers: '-', '(', space or blank
-    trailneg:optional trailing minus indicator:  '-', ')', space or blank
+def clean_postcode(postcode, size=8):
+    assert isinstance(postcode, (str, unicode, int)), 'postcode must been an unicode, string or int'
 
-    >>> d = Decimal('-1234567.8901')
-    >>> moneyfmt(d, curr='$')
-    '-$1,234,567.89'
-    >>> moneyfmt(d, places=0, sep='.', dp='', neg='', trailneg='-')
-    '1.234.568-'
-    >>> moneyfmt(d, curr='$', neg='(', trailneg=')')
-    '($1,234,567.89)'
-    >>> moneyfmt(Decimal(123456789), sep=' ')
-    '123 456 789.00'
-    >>> moneyfmt(Decimal('-0.02'), neg='<', trailneg='>')
-    '<0.02>'
+    cleaned_postcode = re.sub("[^0-9]", "", str(postcode))
+    cleaned_postcode = cleaned_postcode.rjust(size, '0')
 
-    """
-    q = Decimal(10) ** -places      # 2 places --> '0.01'
-    sign, digits, exp = value.quantize(q).as_tuple()
-    result = []
-    digits = map(str, digits)
-    build, next = result.append, digits.pop
-    if sign:
-        build(trailneg)
-    for i in range(places):
-        build(next() if digits else '0')
-    build(dp)
-    if not digits:
-        build('0')
-    i = 0
-    while digits:
-        build(next())
-        i += 1
-        if i == 3 and digits:
-            i = 0
-            build(sep)
-    build(curr)
-    build(neg if sign else pos)
-    return ''.join(reversed(result))
+    return cleaned_postcode
+
+
+def iso_2_utf(text):
+    assert isinstance(text, (str, unicode)), 'text must been a string or unicode'
+
+    return text.decode('iso-8859-1').encode('utf8')
+
+
+def utf_2_iso(text):
+    assert isinstance(text, (str, unicode)), 'text must been a string or unicode'
+
+    return text.decode('utf-8').encode('iso-8859-1')
